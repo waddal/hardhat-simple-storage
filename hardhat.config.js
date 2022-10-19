c126bbc61f692ea1b0213258d42328f22ef509e9
scriptsalmon@gmail.com <scriptsalmon@gmail.com>
2022-10-19 15:41:32 -0700

    defaultNetwork explicitly defined, networks param populated - deploy succeeds

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,19 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-toolbox")
+require("dotenv").config()
+
+const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY
+
+const GOERLI_PRIVATE_KEY = process.env.GOERLI_PRIVATE_KEY
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.17",
-};
+    defaultNetwork: "hardhat",
+    solidity: "0.8.17",
+    networks: {
+        goerli: {
+            url: `https://eth-goerli.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+            accounts: [GOERLI_PRIVATE_KEY],
+            chainId: 5,
+        },
+    },
+}
